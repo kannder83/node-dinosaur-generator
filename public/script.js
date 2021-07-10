7e5182dbfa0ea812b3b7be4d6d2cb74960dfeb51
Alejandro Ruiz <kannder.ziur@gmail.com>
2021-07-10 11:32:10 -0500

    Se agrega la seguridad para no publicar las API KEY.

---
+++ public/script.js
@@ -0,0 +1,33 @@
+console.log("Carga correctamente");
+
+const getDinoName = async () => {
+  const response = await fetch("/dinoname");
+  const data = await response.json();
+  let dinoName = data[0].join(" ");
+  document.querySelector("#dinoName").innerHTML = dinoName;
+  console.log(dinoName);
+};
+
+const getDinoImage = async () => {
+  const response = await fetch("/dinoimage");
+  const data = await response.json();
+  let dinoImage = data.value[Math.floor(Math.random() * data.value.length)];
+  let dinoImageUrl = dinoImage.thumbnailUrl;
+  let dinoAlt = dinoImage.name;
+
+  if (document.querySelector("#dinoImage") !== null) {
+    document.querySelector("#dinoImage").remove();
+  }
+
+  let img = document.createElement("img");
+  img.id = "dinoImage";
+  img.src = dinoImageUrl;
+  img.alt = dinoAlt;
+  document.querySelector("body").appendChild(img);
+  console.log(dinoImageUrl);
+};
+
+document.querySelector("#btnLoad").addEventListener("click", () => {
+  getDinoName();
+  getDinoImage();
+});
